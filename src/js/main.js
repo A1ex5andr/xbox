5a7043e3c69f11b99d45971405b47c439794978e
AlexCoderbit <sashayushchak@gmail.com>
2014-09-29 12:54:59 +0300

    nav + head + mem

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+// var sayHello = require('./say-hello');// example of Browserify require
+
+$(document).ready(function() {
+
+    // sayHello();// example of Browserify require
+    // navigation MAIN MENU
+    var navigation = responsiveNav(".nav-collapse", {
+        animate: true,
+        transition: 160,
+        closeOnNavClick: false
+    });
+
+});
